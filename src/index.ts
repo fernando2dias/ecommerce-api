dc9fdcc98680e0d4f19226c1d9d8797c143a8cd8
Fernando Dias <fernando3dias@gmail.com>
2025-01-02 23:33:52 -0300

    feat: Adding user list and create user endpoint.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,33 @@
-import express from "express";
+import express, {Request, Response} from "express";
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) =>{
+let users = [
+    {
+        name: "Fernando Dias Motta",
+        age: 38
+    },
+    {
+        name: "Juliana Michelsen",
+        age: 39
+    }
+];
+
+app.get('/', (req: Request, res: Response) =>{
     res.send("Welcome, Hello World");
+});
+
+app.get("/users", (req: Request, res: Response) => {
+    res.send(users);
+});
+
+app.post("/users", (req: Request, res: Response)=>{
+    let user = req.body;
+    users.push(user);
+    res.send({
+    message: "User was created!"
+    });
 });
 
 app.listen(3000, ()=>{
