8dc4ed402f12e5fc43bd015f4cfd127fe4d3040e
Fernando Dias Motta <fernando.motta@prilltc.onmicrosoft.com>
2025-01-03 15:42:51 -0300

    feat: Adding PUT and Delete Verbs.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,17 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 app.use(express.json());
 
-let users = [
-    {
-        name: "Fernando Dias Motta",
-        age: 38
-    },
-    {
-        name: "Juliana Michelsen",
-        age: 39
-    }
-];
+let id = 1;
+interface User {
+    id: number; 
+    name: string;
+    email: string;
+}
+let users: User[] = [];
 
-app.get('/', (req: Request, res: Response) =>{
+app.get('/', (req: Request, res: Response) => {
     res.send("Welcome, Hello World");
 });
 
@@ -22,14 +19,54 @@ app.get("/users", (req: Request, res: Response) => {
     res.send(users);
 });
 
-app.post("/users", (req: Request, res: Response)=>{
-    let user = req.body;
-    users.push(user);
+app.get("/users/:id", (req: Request, res: Response) => {
+    let userId = Number(req.params.id);
+    let user = users.find(user => user.id === userId);
+    res.send(user);
+});
+
+app.delete("/users/:id", (req: Request, res: Response) => {
+    let userId = Number(req.params.id);
+    let userIndex = users.findIndex((user) => user.id === userId);
+
+    if (userIndex === -1) {
+        res.status(404).send({ message: "User not found" });
+    }
+
+    users.splice(userIndex, 1);
+
     res.send({
-    message: "User was created!"
+        message: "User was deleted!"
     });
 });
 
-app.listen(3000, ()=>{
+app.put("/users/:id", (req: Request, res: Response) => {
+    let userId = Number(req.params.id);
+    let userEdited = req.body;
+    let userIndex = users.findIndex((user) => user.id === userId);
+
+    if (userIndex === -1) {
+        res.status(404).send({ message: "User not found" });
+    }
+
+    users[userIndex] = {
+         id: userId,
+         name: userEdited.name,
+         email: userEdited.email,
+    };
+
+    res.send({message: "User updated successfully"});
+});
+
+app.post("/users", (req: Request, res: Response) => {
+    let user = req.body;
+    user.id = id++;
+    users.push(user);
+    res.send({
+        message: "User was created!"
+    });
+});
+
+app.listen(3000, () => {
     console.log("Server is active at port 3000");
 });
