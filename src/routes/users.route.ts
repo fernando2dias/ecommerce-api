a9b121d4872b15280caaa944659a0432cfb7ee99
Fernando Dias Motta <fernando.motta@prilltc.onmicrosoft.com>
2025-01-04 17:14:15 -0300

    feat: added users controllers.

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,63 +1,14 @@
-import express, {Request, Response} from "express";
+import express from "express";
+import { UsersController } from "../controllers/users.controller";
 
 export const userRouters = express.Router();
 
-let id = 1;
-interface User {
-    id: number; 
-    name: string;
-    email: string;
-}
-let users: User[] = [];
+userRouters.get("/users", UsersController.getAll);
 
-userRouters.get("/users", (req: Request, res: Response) => {
-    res.send(users);
-});
+userRouters.get("/users/:id", UsersController.getById);
 
-userRouters.get("/users/:id", (req: Request, res: Response) => {
-    let userId = Number(req.params.id);
-    let user = users.find(user => user.id === userId);
-    res.send(user);
-});
+userRouters.delete("/users/:id", UsersController.delete);
 
-userRouters.delete("/users/:id", (req: Request, res: Response) => {
-    let userId = Number(req.params.id);
-    let userIndex = users.findIndex((user) => user.id === userId);
+userRouters.put("/users/:id", UsersController.update);
 
-    if (userIndex === -1) {
-        res.status(404).send({ message: "User not found" });
-    }
-
-    users.splice(userIndex, 1);
-
-    res.send({
-        message: "User was deleted!"
-    });
-});
-
-userRouters.put("/users/:id", (req: Request, res: Response) => {
-    let userId = Number(req.params.id);
-    let userEdited = req.body;
-    let userIndex = users.findIndex((user) => user.id === userId);
-
-    if (userIndex === -1) {
-        res.status(404).send({ message: "User not found" });
-    }
-
-    users[userIndex] = {
-         id: userId,
-         name: userEdited.name,
-         email: userEdited.email,
-    };
-
-    res.send({message: "User updated successfully"});
-});
-
-userRouters.post("/users", (req: Request, res: Response) => {
-    let user = req.body;
-    user.id = id++;
-    users.push(user);
-    res.send({
-        message: "User was created!"
-    });
-});
+userRouters.post("/users", UsersController.save);
