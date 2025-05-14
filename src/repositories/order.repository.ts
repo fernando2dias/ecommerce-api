2b2c8494963b71ccf566d172fe2fbc151c92dbb1
Fernando Dias Motta <fernando.motta@prill.com.br>
2025-05-13 21:51:52 -0300

    configurando filtro de data

--- src/repositories/order.repository.ts
+++ src/repositories/order.repository.ts
@@ -1,5 +1,8 @@
 import { CollectionReference, getFirestore } from "firebase-admin/firestore";
 import { Order, QueryParamsOrder } from "../models/order.model.js";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc.js";
+import timezone from 'dayjs/plugin/timezone.js';
 
 export class OrderRepository {
     private collection: CollectionReference;
@@ -17,6 +20,30 @@ export class OrderRepository {
 
         if(queryParams.companyId) {
           query = query.where("company.id", "==", queryParams.companyId);
+        }
+
+        
+        dayjs.extend(utc);
+        dayjs.extend(timezone);
+        const tz = "America/Sao_Paulo";
+        dayjs.tz.setDefault("America/Sao_Paulo");
+
+
+        if(queryParams.beginDate) {
+            console.log("queryParams.beginDate", queryParams.beginDate);
+            queryParams.beginDate = dayjs(queryParams.beginDate).add(1,'day').startOf('day').toDate();
+            queryParams.beginDate = dayjs.tz(queryParams.beginDate).tz(tz, true).toDate();
+            console.log("queryParams.beginDate", queryParams.beginDate);
+            query = query.where("date", ">=", new Date(queryParams.beginDate));
+            
+
+        }
+
+        if(queryParams.endDate) {
+            console.log("queryParams.endDate", queryParams.endDate);
+            queryParams.endDate = dayjs.tz(queryParams.endDate).add(1, 'day').endOf('day').toDate();
+            console.log("queryParams.endDate", queryParams.endDate);
+            query = query.where("date", "<=", new Date(queryParams.endDate));
         }
 
         if(queryParams.status){
